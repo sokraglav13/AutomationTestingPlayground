fc65b3de2bbc2e85bc222eddb73121c2f64f98c8
sokgla12 <sokraglav@gmail.com>
2023-08-28 13:05:52 +0300

    28_08_2023_4 - minor changes

---
+++ Pages/ErrorMessages.js
@@ -0,0 +1,4 @@
+module.exports = {
+    ElementClickInterceptedError: "element click intercepted: Element <button class=\"btn btn-success\" type=\"button\" id=\"green...\">Button</button> is not clickable at point (441, 416). Other element would receive the click: <button type=\"button\" class=\"btn btn-primary\" id=\"blue...\">Button</button>\n" +
+        "  (Session info: chrome=115.0.5790.102)"
+};
